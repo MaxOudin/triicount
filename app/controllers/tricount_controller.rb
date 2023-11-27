8f00e3afed8df55eb81bda9adb7fec5c9e1dd0d3
Maxime Oudin <oudinmaxime@gmail.com>
2023-11-27 14:56:39 +0100

    tricounts nested in user and tricount creation form visible

--- app/controllers/tricount_controller.rb
+++ app/controllers/tricount_controller.rb
@@ -1,4 +1,6 @@
 class TricountController < ApplicationController
+  before_action :find_user_id
+  before_action :authenticate_user!
 
   def new
     @tricount = Tricount.new
@@ -6,16 +8,21 @@ class TricountController < ApplicationController
 
   def create
     @tricount = Tricount.new(tricount_params)
+    @tricount.user = @user
     if @tricount.save
-      redirect_to @tricount, notice: 'Triicount was successfully created.'
+      redirect_to tricounts_path(@user), notice: 'Triicount was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
+  def find_user_id
+    @user = User.find(params[:user_id])
+  end
+
   def tricount_params
-    params.require(:tricount).permit(:title, :description, :participants)
+    params.require(:tricount).permit(:user, :title, :description, :participants, :user_id)
   end
 end
