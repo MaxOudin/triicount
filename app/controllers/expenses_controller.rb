b057908ce1e15b302237b117e44357fad2a5d7f9
Maxime Oudin <oudinmaxime@gmail.com>
2023-11-29 10:30:39 +0100

    form for expense creation done and route available

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,26 @@
 class ExpensesController < ApplicationController
+  before_action :find_tricount_id
 
   def index
     @expenses = Expense.all
   end
 
-  def method_name
-    
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new
+  end
+
+  private
+
+  def find_tricount_id
+    @tricount = Tricount.find(params[:tricount_id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:tricount, :title, :description, :amount_cents, :payer, :debtors)
   end
 
 end
