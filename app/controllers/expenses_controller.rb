d6868f2afc3e0d724982c76a7554f4f9ce000786
Maxime Oudin <oudinmaxime@gmail.com>
2023-11-29 12:02:36 +0100

    form for expense can be submitted and saved for tricount

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -10,17 +10,30 @@ class ExpensesController < ApplicationController
   end
 
   def create
-    @expense = Expense.new
+    @expense = Expense.new(expense_params)
+    @expense.tricount_id = @tricount.id
+    if @expense.save
+      redirect_to user_tricounts_path(@tricount), notice: 'Expense was successfully created.'
+    else
+      flash[:alert] = 'Expense could not be created'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def find_tricount_id
-    @tricount = Tricount.find(params[:tricount_id])
+    @tricount = Tricount.find_by(id: params[:tricount_id])
+    @user = @tricount.user
+
+    unless @tricount
+      flash[:alert] = 'Tricount not found'
+      redirect_to root_path
+    end
   end
 
   def expense_params
-    params.require(:expense).permit(:tricount, :title, :description, :amount_cents, :payer, :debtors)
+    params.require(:expense).permit(:user_id, :tricount_id, :title, :description)
   end
 
 end
